3d4e81982d9920a38e2328bf0cde70ce4f10dd96
Chengchcc <chen@aion.network>
2020-04-03 14:55:08 +0800

    remove workspace

--- packages/base-connector/src/index.ts
+++ packages/base-connector/src/index.ts
@@ -49,6 +49,9 @@ export class Deferred<T> implements Promise<T> {
     ): Promise<T | TResult> {
         return this.promise.catch(onrejected);
     }
+    finally(onfinally?: (() => void) | null | undefined): Promise<T> {
+        return this.promise.finally(onfinally);
+    }
 }
 
 type Callback = (...data: any[]) => any;
